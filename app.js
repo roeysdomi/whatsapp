d1d6989cb155e759aebf0c64bea1eed0b9290eb5
Roey <roeysdomi@gmail.com>
2021-02-05 03:20:56 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless:false})
+  const page = await browser.newPage()
+  await page.goto('https://www.google.com/')
+  page.on('dialog', async dialog => {
+    console.log(dialog.message())
+    await dialog.dismiss()
+  })
+  await page.evaluate(() => alert('This message is inside an alert box'))
+  await browser.close()
+})()
